30dd62a5f57279de0bbd484ce63593e477a6b14b
Sharker5854 <kiryanov_vova_134@mail.ru>
2021-09-14 22:11:53 +0300

    Fixed some front-end bugs on index page AND added a description block for videos.

--- hubinter/hubinter/static/js/script.js
+++ hubinter/hubinter/static/js/script.js
@@ -10,7 +10,7 @@ function show_theme_tags_and_videos(theme) {
     scroll_down_hint.style.display = "none"
 
     for (let video of videos) { // show all videos (accord to current theme)
-        video.style.opacity = "1"
+        video.style.display = "list-item"
     }
     for (let tag_btn of tag_buttons) { // disable all tags
         tag_btn.classList.remove('selected-tag')
@@ -59,7 +59,7 @@ function show_videos_by_tag(tag_name) {
             for (let video of videos) {          // (accordingly, the videos of the disabled tags are hidden)
                 if( video.classList.contains(tag_name) ) { // just show videos of current tag
                     scroll_down_hint.style.display = "block"
-                    video.style.opacity = "1"
+                    video.style.display = "list-item"
                 }
             }
         }
@@ -67,7 +67,7 @@ function show_videos_by_tag(tag_name) {
             for (let video of videos) {
                 if( !video.classList.contains(tag_name) ) {
                     scroll_down_hint.style.display = "block"
-                    video.style.opacity = "0" // hiding all tag's videos except the current one
+                    video.style.display = "none" // hiding all tag's videos except the current one
                 }
             }
         }
@@ -89,14 +89,14 @@ function show_videos_by_tag(tag_name) {
         if (already_selected_tags_counter > 0) { // if there are some already enabled tags
             for (let video of videos) {          // (accordingly, the videos of the enabled tags are shown)
                 if( video.classList.contains(tag_name) ) { // just hide videos of current tag
-                    video.style.opacity = "0"
+                    video.style.display = "none"
                 }
             }
         }
         else { // if NO tag is already enabled (WE DISABLED LAST TAG ABOVE)
             for (let video of videos) { // just show all videos
                 scroll_down_hint.style.display = "none"
-                video.style.opacity = "1"
+                video.style.display = "list-item"
             }
         }
 
